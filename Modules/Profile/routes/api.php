e8276f2c8c19eb586fc9be8c5d923aba06e5e073
saeed pasa <sp.najafian@gmail.com>
2025-10-21 13:25:25 +0330

    Delete extra module

--- Modules/Profile/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Profile\Http\Controllers\ProfileController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('profiles', ProfileController::class)->names('profile');
-});
--- Modules/Profile/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Profile\Http\Controllers\ProfileController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('profiles', ProfileController::class)->names('profile');
-});
